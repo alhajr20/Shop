961fbcb0f82441890897ed07666d96ad408f3c43
Khalid <alhajr20@gmail.com>
2021-06-27 15:57:17 +0300

    Fixed bags

--- dist/js/sign-up.js
+++ dist/js/sign-up.js
@@ -1,13 +1,38 @@
 "use strict";
 
-var privacyCheckbox = document.querySelector('input[type="checkbox"]'),
-    registerSubmitBtn = document.querySelector('.register-submit');
-privacyCheckbox.addEventListener('click', function (e) {
-  var elem = e.target;
+window.addEventListener('DOMContentLoaded', function () {
+  var menu = document.querySelector('.nav__mobile'),
+      openBtn = document.querySelector('.hamburger'),
+      closeBtn = document.querySelector('button.close');
+  openBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu.classList.add('mobileActive');
+  });
+  closeBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu.classList.remove('mobileActive');
+  });
+  var privacyCheckbox = document.querySelector('input[type="checkbox"]'),
+      registerSubmitBtn = document.querySelector('.register-submit');
+  privacyCheckbox.addEventListener('click', function (e) {
+    var elem = e.target;
 
-  if (elem.checked) {
-    registerSubmitBtn.removeAttribute('disabled');
-  } else {
-    registerSubmitBtn.setAttribute('disabled', '');
-  }
+    if (elem.checked) {
+      registerSubmitBtn.removeAttribute('disabled');
+    } else {
+      registerSubmitBtn.setAttribute('disabled', '');
+    }
+  });
+  var upBtn = document.querySelector('.up');
+  window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop > 150) {
+      upBtn.classList.add('upShow');
+    } else {
+      upBtn.classList.remove('upShow');
+    }
+  });
+  upBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo(pageXOffset, 0);
+  });
 });
--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -1,12 +1,44 @@
-const privacyCheckbox = document.querySelector('input[type="checkbox"]'),
-      registerSubmitBtn = document.querySelector('.register-submit');
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav__mobile'),
+          openBtn = document.querySelector('.hamburger'),
+          closeBtn = document.querySelector('button.close');
 
-privacyCheckbox.addEventListener('click', (e) => {
-    const elem = e.target;
+    openBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        menu.classList.add('mobileActive');
+    });
 
-    if (elem.checked) {
-        registerSubmitBtn.removeAttribute('disabled');
-    } else {
-        registerSubmitBtn.setAttribute('disabled', '');
-    }
+    closeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        menu.classList.remove('mobileActive');
+    });
+
+    const privacyCheckbox = document.querySelector('input[type="checkbox"]'),
+          registerSubmitBtn = document.querySelector('.register-submit');
+
+    privacyCheckbox.addEventListener('click', (e) => {
+        const elem = e.target;
+
+        if (elem.checked) {
+            registerSubmitBtn.removeAttribute('disabled');
+        } else {
+            registerSubmitBtn.setAttribute('disabled', '');
+        }
+    });
+
+    const upBtn = document.querySelector('.up');
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 150) {
+            upBtn.classList.add('upShow');
+        } else {
+            upBtn.classList.remove('upShow');
+        }
+    });
+
+    upBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        window.scrollTo(pageXOffset, 0);
+    });
 });
